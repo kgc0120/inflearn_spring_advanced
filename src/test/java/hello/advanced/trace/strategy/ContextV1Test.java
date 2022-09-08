02965d379a3703047ab77b729d5c0dd7dfcd2b65
GyuCheol <senru0508@gmail.com>
2022-09-08 15:43:29 +0900

    전략 패턴 익명 내부 클래스, 람다

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogin1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogin2;
 import hello.advanced.trace.template.code.AbstractTemplate;
@@ -45,6 +46,9 @@ public class ContextV1Test {
 
     /**
      * 전략 패턴 사용
+     * 선 조립, 후 실행
+     * 스프링 애플리케이션 개발 할 때 애플리케이션 로딩 시점에 의존관계 주입을 통해 필요한 의존관계를 모두 맺어두고
+     * 다음에 실제 요청을 처리하는 것과 같은 원리이다.
      */
     @Test
     void strategyV1() {
@@ -55,7 +59,61 @@ public class ContextV1Test {
         StrategyLogin2 strategyLogin2 = new StrategyLogin2();
         ContextV1 contextV2 = new ContextV1(strategyLogin2);
         contextV2.execute();
+    }
 
+    @Test
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
+
+    }
+
+    @Test
+    void strategyV3() {
+        ContextV1 contextV1 = new ContextV1(new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+        contextV2.execute();
+
+    }
+
+    @Test
+    void strategyV4() {
+        ContextV1 contextV1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        contextV2.execute();
 
     }
 
